7133f84a799934a7de7a3b900ad125fb0d8bf230
rishabhdevyadav <rishabhdevyadav95@gmail.com>
2020-09-25 18:15:28 +0530

    c++ oops

--- oopspyhton/STANDARD.py
+++ oopspyhton/STANDARD.py
@@ -15,18 +15,20 @@ class Cuboid:
     def lateral_surface_area(self):
     	return 2*self.h*(self.l + self.b)
 
-    def volume(self):
-    	return self.l * self.b * self.h
+    def volume(self, x):  #x = 10 times of volume
+    	return self.l * self.b * self.h * x
 
-    def diagonal_legth(self):
+    def diagonal_legth(self, x, y, z): #x,y,z are random input
     	return math.sqrt(self.l*self.l + self.b*self.b + self.h*self.h)
 
 
 if __name__ == '__main__':
+
  	cuboid1 = Cuboid(2,3,4)
+
  	tsa1 = cuboid1.total_surface_area()
  	lsa1 = cuboid1.lateral_surface_area()
- 	v1 = cuboid1.volume()
- 	dl1 = cuboid1.diagonal_legth()
+ 	v1 = cuboid1.volume(10)  #x = 10 times of volume
+ 	dl1 = cuboid1.diagonal_legth(1,1,1)
 
  	print(tsa1, lsa1, v1, dl1)
